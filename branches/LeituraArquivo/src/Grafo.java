9a353e0326dd51d7551d73f01150361dbd31b3d5
franzejr@gmail.com <franzejr@gmail.com@5da559a9-3a4f-77a5-d58b-6de179e295d9>
2011-05-19 16:48:24 +0000

    A parte do arquivo está funcionando!!

--- branches/LeituraArquivo/src/Grafo.java
+++ branches/LeituraArquivo/src/Grafo.java
@@ -6,53 +6,107 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Grafo {
 
-	Map<Vertice, List<Vertice>> la = new HashMap<Vertice, List<Vertice>>();
-	
-	//Atributos usados na função encontrarMenorCaminho
-    
-    //Lista que guarda os vértices pertencentes ao menor caminho encontrado
-    List<Vertice> menorCaminho = new ArrayList<Vertice>();
-    
-    //Variável que recebe os vértices pertencentes ao menor caminho
-    Vertice verticeCaminho = new Vertice ("caminho");
-    
-    //Variável que guarda o vértice que está sendo visitado 
-    Vertice atual = new Vertice ("atual");
-    
-    //Variável que marca o vizinho do vértice atualmente visitado 
-    Vertice vizinho = new Vertice ("vizinho");
-    
-    //Corte de vértices que já tiveram suas distâncias marcadas e cujos vizinhos não foram visitados
-    List<Vertice> fronteira = new ArrayList<Vertice>();
-    
-    //Guarda o número de vértices não visitados
-    int verticesNaoVisitados;
+	List<Vertice> grafo = new ArrayList<Vertice>();
 
+	// Atributos usados na função encontrarMenorCaminho
 
-	public static Map<Vertice, List<Vertice>> lerGrafo(String nomeArquivo) {
+	// Lista que guarda os vértices pertencentes ao menor caminho encontrado
+	List<Vertice> menorCaminho = new ArrayList<Vertice>();
+
+	// Variável que recebe os vértices pertencentes ao menor caminho
+	Vertice verticeCaminho = new Vertice("caminho");
+
+	// Variável que guarda o vértice que está sendo visitado
+	Vertice atual = new Vertice("atual");
+
+	// Variável que marca o vizinho do vértice atualmente visitado
+	Vertice vizinho = new Vertice("vizinho");
+
+	// Corte de vértices que já tiveram suas distâncias marcadas e cujos
+	// vizinhos não foram visitados
+	List<Vertice> fronteira = new ArrayList<Vertice>();
+
+	// Guarda o número de vértices não visitados
+	int verticesNaoVisitados;
+
+	public static List<Vertice> lerGrafo(String nomeArquivo) {
+
+		Grafo g = new Grafo();
+		Vertice v;
 		File f = new File(nomeArquivo);
-		Map<Vertice, List<Vertice>> la = new HashMap<Vertice, List<Vertice>>();
+		String vertices[];
+		String linha;
+		ArrayList<String[]> s1 = new ArrayList<String[]>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(f));
-			String linha;
 
 			while ((linha = br.readLine()) != null) {
-				Vertice v = new Vertice();
-				String[] token = linha.split("(:)|(,)");
-				List<Vertice> adj = new ArrayList<Vertice>();
-				for (int i = 1; i < token.length; i++) {
-					adj.add(v);
-					adj.get(i).setDescricao(token[i]);
+
+				if (linha.contains(",")) {
+					s1.add(linha.split("/"));
+					System.out.println(s1.get(0)[0]);
+
+					// vertices = s1.get(0)[0].split(",");
+					vertices = s1.get(0)[0].split(",");
+
+					System.out.println(s1.get(0)[1]);
+
+					v = new Vertice();
+					List<Vertice> vizinhosAtual = new ArrayList<Vertice>();
+					List<Aresta> arestasAtual = new ArrayList<Aresta>();
+					v.setDescricao(vertices[0]);
+
+					if (linha.contains("/")) {
+
+						String pesoArestas[] = s1.get(0)[1].split(",");
+
+						for (int i = 1; i < vertices.length; i++) {
+							Vertice vit = new Vertice();
+							System.out.println(vertices[i]);
+							// System.out.println("tamanho"+ s1[0].length());
+							vit.setDescricao(vertices[i]);
+							vizinhosAtual.add(vit);
+
+							v.setVizinhos(vizinhosAtual);
+
+							Aresta ait = new Aresta(v, vit);
+							ait.setPeso(Integer.parseInt(pesoArestas[i - 1]));
+							arestasAtual.add(ait);
+
+						}
+
+						v.setArestas(arestasAtual);
+
+					}
+
+					System.out.println("vertice = " + v.getDescricao());
+					// Teste
+					for (Aresta a : arestasAtual) {
+
+						System.out.println("peso " + a.getPeso());
+					}
+
 				}
-				Vertice vInicial = new Vertice();
-				vInicial.setDescricao(token[0]);
-				la.put(vInicial, adj);
+
+				// Vertices finais
+				else {
+					v = new Vertice();
+					v.setDescricao(linha);
+
+				}
+
+				g.grafo.add(v);
+				s1.clear();
+				System.out.println("desgraçado limpo:" + s1.size());
+				System.out.printf("\n");
 			}
+
 			// catch do BufferedReader
 		} catch (FileNotFoundException e) {
 			System.out.println("Nao encontrou o arquivo");
@@ -64,24 +118,33 @@ public class Grafo {
 			e.printStackTrace();
 		}
 
-		return la;
+		return g.grafo;
 	}
 
 	public static void main(String args[]) {
+		Grafo teste = new Grafo();
+		
+		teste.grafo = lerGrafo("teste.txt");
+		int i1=0,i2 = 0;
+		for(int i = 0; i < teste.grafo.size(); i++){
+			if(teste.grafo.get(i).getDescricao().equals("v1")) i1 = i;
+			if(teste.grafo.get(i).getDescricao().equals("v3")) i2 = i;
+		}
+		
+		List<Vertice> resultado = new ArrayList<Vertice>();
+		
+		resultado = teste.encontrarMenorCaminhoDijkstra(teste, teste.grafo.get(i1), teste.grafo.get(i2));
 
-		Grafo g = new Grafo();
-
-		g.la = lerGrafo("teste.txt");
-
-		System.out.println(g.la);
-
+		
+			System.out.println(resultado);
 	}
 
 	// Algoritmo de Dijkstra
-	public List<Vertice> encontrarMenorCaminhoDijkstra(Vertice v1, Vertice v2) {
+	public List<Vertice> encontrarMenorCaminhoDijkstra(Grafo g, Vertice v1,
+			Vertice v2) {
 
 		// No início, todos os vértices do grafo não foram visitados
-		verticesNaoVisitados = this.la.size();
+		verticesNaoVisitados = g.grafo.size();
 
 		// O primeiro nó a ser visitado é o da origem do caminho
 		atual = v1;
@@ -91,34 +154,38 @@ public class Grafo {
 		menorCaminho.add(atual);
 
 		// Colocando a distancias iniciais
-		for (int i = 0; i < this.la.size(); i++) {
+		for (int i = 0; i < g.grafo.size(); i++) {
 
 			// Nó atual tem distância zero, e todos os outros, 9999(infinita)
-			if (this.la.containsValue(atual.getDescricao())) {
-				
-			 ((Vertice) this.la.get(i)).getDescricao();
-				//this.vertices.get(i).setDistancia(0);
+			if (g.grafo.get(i).getDescricao()
+					.equals(atual.getDescricao())) {
+
+				g.grafo.get(i).setDistancia(0);
+			
 
 			} else {
 
-				((Vertice) this.la.get(i)).setDistancia(9999);
+				g.grafo.get(i).setDistancia(9999);
 
 			}
 		}
 
 		// O algoritmo continua até que todos os vértices sejam visitados
+		fronteira = grafo;
+		Collections.sort(fronteira);
 		while (verticesNaoVisitados != 0) {
 
 			// Toma-se sempre o vértice com menor distância, que é o primeiro da
 			// lista do corte
-			atual = this.fronteira.get(0);
+			atual = fronteira.get(0);
+			//atual = this.fronteira.get(0);
 			/*
 			 * Para cada vizinho (cada aresta), calcula-se a sua possível
 			 * distância, somando a distância do vértice atual com a da aresta
 			 * correspondente. Se essa distância for menor que a distância do
 			 * vizinho, esta é atualizada.
 			 */
-			for (int i = 0; i < la.size(); i++) {
+			for (int i = 0; i < atual.getArestas().size(); i++) {
 
 				vizinho = atual.getArestas().get(i).getDestino();
 				if (!vizinho.verificarVisita()) {
--- branches/LeituraArquivo/src/Vertice.java
+++ branches/LeituraArquivo/src/Vertice.java
@@ -1,18 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Vertice {
+
+public class Vertice implements Comparable<Vertice> {
 	
 	private String descricao;
     private int distancia;
     private boolean visitado = false;
     private Vertice pai;
+    private List<Aresta> arestas = new ArrayList<Aresta>();
+    private List<Vertice> vizinhos = new ArrayList<Vertice>();
+    
+    
     
     public Vertice(String s){
     	this.descricao = s;
     }
     
     public Vertice(){
-    	
+    
     }
+    
+    public void visitar (){
+        
+        this.visitado = true;
+    }
+    
+    public boolean verificarVisita(){
+        
+        return visitado;
+    }
+    
 	public String getDescricao() {
 		return descricao;
 	}
@@ -36,6 +56,46 @@ public class Vertice {
 	}
 	public void setPai(Vertice pai) {
 		this.pai = pai;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Vertice){
+			Vertice vRef = (Vertice) obj;
+			if(this.getDescricao().equals(vRef.getDescricao())) return true;
+		}
+		return false;
+	}
+
+	@Override
+	public int compareTo(Vertice vertice) {
+		if(this.getDistancia() < vertice.getDistancia()) return -1;
+		else if(this.getDistancia() == vertice.getDistancia()) return 0;
+		
+		return 1;
+	}
+	
+	@Override
+	public String toString() {
+		String s = " ";
+		s+= this.getDescricao();
+		return s;
+	}
+
+	public void setArestas(List<Aresta> arestas) {
+		this.arestas = arestas;
+	}
+
+	public List<Aresta> getArestas() {
+		return arestas;
+	}
+
+	public void setVizinhos(List<Vertice> vizinhos) {
+		this.vizinhos = vizinhos;
+	}
+
+	public List<Vertice> getVizinhos() {
+		return vizinhos;
 	}
 
 }
